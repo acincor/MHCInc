c9e5bf0875505961c4171ac1a73ee812e606563e
Mhc-inc <Wf6350177@163.com>
2022-11-13 07:24:25 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.rst", "r",encoding = "UTF-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MHCInc",
+    version="0.0.5",
+    author="Mhc-inc",
+    author_email="Wf6350177@163.com",
+    description="update from Swift-module-copiseded",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/MHCInc/",
+    project_urls={
+        "Bug Tracker": "https://github.com/Mhc-inc/swift-module-copiseded-new-/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"MHCInc": "MHCInc"},
+    packages=setuptools.find_packages(where="MHCInc"),
+    python_requires=">=3.9",
+)
