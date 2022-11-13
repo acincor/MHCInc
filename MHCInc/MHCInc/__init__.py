1fd86718ccfd73571002fee3fcc4cf3cbb6f9a5b
Mhc-inc <Wf6350177@163.com>
2022-11-13 08:38:02 +0800

    end commit

--- MHCInc/MHCInc/__init__.py
+++ MHCInc/MHCInc/__init__.py
@@ -1,65 +1,65 @@
 import subprocess
 import requests
 class module:
-    class game:
-        def main():
-        print("This app is Copyright © MHC Inc in 2022.5.22.13:53, This is The sixth app\nMIT License\nCopyright © 2022 monkeyhammercopiseded\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the \"Software\"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.")
-    def caterpillar():
-        from MHCInc import caterpillar
-    def 单词九连猜():
-        from MHCInc import 单词九连猜
-    def nineWordGuessing():
-        from MHCInc import 单词九连猜
-    def 加密消息():
-        from MHCInc import 加密消息
-    def secretMessage():
-        from MHCInc import 加密消息
-    def snap():
-        from MHCInc import 快照抓拍
-    def 快照抓拍():
-        from MHCInc import 快照抓拍
-    def 螺旋万花筒():
-        from MHCInc import 螺旋万花筒
-    def spiralKaleidoscope():
-        from MHCInc import 螺旋万花筒
-    def 星光夜空():
-        from MHCInc import 星光夜空
-    def starlightNight():
-        from MHCInc import 星光夜空
-    def 圆筒万花筒():
-        from MHCInc import 圆筒万花筒
-    def cylinderKaleidoscope():
-        from MHCInc import 圆筒万花筒
-    def animal_guess():
-        from MHCInc import animal_guess
-    def caterpillar2():
-        from MHCInc import caterpillar2
-    def copyright():
-        from MHCInc import copyright
-    def eggCatcherPerfect():
-        from MHCInc import egg_catcher_perfect
-    def eggCatcher():
-        from MHCInc import egg_catcher
-    def GuessGame():
-        from MHCInc import GuessGame
-    def GuessGameEasy():
-        from MHCInc import GuessGameEasy
-    def GuessGameSecondary():
-        from MHCInc import GuessGameSecondary
-    def matchmaker():
-        from MHCInc import matchmaker
-    def MITLicense():
-        from MHCInc import MITLicense
-    def PHYSICAL_CONDITION_APPLET():
-        from MHCInc import PHYSICAL_CONDITION_APPLET
-    def Python3学习示例():
-        from MHCInc import Python3学习示例
-    from MHCInc import random
-    def rectangle():
-        from MHCInc import rectangle
-    def robotBuilder():
-        from MHCInc import robot_builder
-    def screenPet():
-        from MHCInc import screen_pet
-    from MHCInc import swift
+	def main():
+		from MHCInc import copyright
+		copyright.copyright_Swift()
+	def caterpillar():
+		from MHCInc import caterpillar
+	def 单词九连猜():
+		from MHCInc import 单词九连猜
+	def nineWordGuessing():
+		from MHCInc import 单词九连猜
+	def 加密消息():
+		from MHCInc import 加密消息
+	def secretMessage():
+		from MHCInc import 加密消息
+	def snap():
+		from MHCInc import 快照抓拍
+	def 快照抓拍():
+		from MHCInc import 快照抓拍
+	def 螺旋万花筒():
+		from MHCInc import 螺旋万花筒
+	def spiralKaleidoscope():
+		from MHCInc import 螺旋万花筒
+	def 星光夜空():
+		from MHCInc import 星光夜空
+	def starlightNight():
+		from MHCInc import 星光夜空
+	def 圆筒万花筒():
+		from MHCInc import 圆筒万花筒
+	def cylinderKaleidoscope():
+		from MHCInc import 圆筒万花筒
+	def animal_guess():
+		from MHCInc import animal_guess
+	def caterpillar2():
+		from MHCInc import caterpillar2
+	def copyright():
+		from MHCInc import copyright
+	def eggCatcherPerfect():
+		from MHCInc import egg_catcher_perfect
+	def eggCatcher():
+		from MHCInc import egg_catcher
+	def GuessGame():
+		from MHCInc import GuessGame
+	def GuessGameEasy():
+		from MHCInc import GuessGameEasy
+	def GuessGameSecondary():
+		from MHCInc import GuessGameSecondary
+	def matchmaker():
+		from MHCInc import matchmaker
+	def MITLicense():
+		from MHCInc import MITLicense
+	def PHYSICAL_CONDITION_APPLET():
+		from MHCInc import PHYSICAL_CONDITION_APPLET
+	def Python3学习示例():
+		from MHCInc import Python3学习示例
+	from MHCInc import random
+	def rectangle():
+		from MHCInc import rectangle
+	def robotBuilder():
+		from MHCInc import robot_builder
+	def screenPet():
+		from MHCInc import screen_pet
+	from MHCInc import swift
 
