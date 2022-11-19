984f5693818c7c026b14f25a645f5c53e98f88f3
Mhc-inc <Wf6350177@163.com>
2022-11-19 17:55:20 +0800

    end commit

--- MHCInc/MHCInc/__init__.py
+++ MHCInc/MHCInc/__init__.py
@@ -2,11 +2,16 @@ import subprocess
 import requests
 import sys
 import requests
+import os
 version = str(sys.version_info.major)+"."+str(sys.version_info.minor)
-with open("/Library/Frameworks/Python.framework/Versions/{0}/lib/python{0}/site-packages/MHCInc/200bpm%E6%AD%BB%E4%BA%A1%E4%B9%8B%E6%9B%B2 2.flac".format(version),"wb") as f:
-        f.write(requests.get("https://cvws.icloud-content.com/B/AYnJLomq015_9JFy_7RFWp_J5ckzAT6Qggg5_Zwi4QgRDko_wiTeoIe4/200bpm%25E6%25AD%25BB%25E4%25BA%25A1%25E4%25B9%258B%25E6%259B%25B2+2.flac?o=AmtJ9a7yBSoLgoNQFsAVInBR0hPSi_qBFXDuhl62D81c&v=1&x=3&a=CAogQ3ePitFEaK7qK-WRIbcVnybPAYGc3Phr6sckWBfaPtoSbxC3j9L4yDAYt-yt-sgwIgEAUgTJ5ckzWgTeoIe4aifMyR4JbAdJJFa8iSr4KCX7v6lRp0WDq4uPxk0qnyk-Nkx7-RxTY2ZyJyEojERp6koR5OBpuIKWtih9yr-HHmZhby-2S2Q-o1Wl3XSTCZRjjw&e=1668851398&fl=&r=7ff60414-3950-4b25-b3cb-917015438bc6-1&k=2l8AzGgk6B0IYGK7rA8voQ&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=71&s=fHy9Bogcajj_t_DG_0u7B5T4VV8").content)
-with open("/Library/Frameworks/Python.framework/Versions/{0}/lib/python{0}/site-packages/MHCInc/8bq.png".format(version),"wb") as f:
-        f.write(requests.get("https://cvws.icloud-content.com/B/Acr_s1UYJJ0kGxK5ohV72-jVci_dAR48DvZzepaA7SCs8vhnB48Pc5Va/8bq.png?o=AlFov2KnHs0gs9InUAmWS_rnsz5S5gMPcfS-JN4dx6jm&v=1&x=3&a=CAoghS0sbGGYFC6tDaTy52_wbZuUjBoD65jXVxCbliSJpBESbxD846b5yDAY_MCC-8gwIgEAUgTVci_dWgQPc5Vaaide9s-0FqVsYCECZvh08Z-dEdMsM6DpV9oljFqQPeauzb7L8i2n33NyJ16-n7asJGc6-pDCjl4Im4IhgIA26P590fjQfY-G8vLOSe-bHqiwvQ&e=1668852785&fl=&r=d26fca4a-88af-419b-94c6-60ef1a5ed7ff-1&k=Y2OcQR3mvMwtdr73YGvuSQ&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=71&s=19M4Vl9Pt5i9BAmiSf2MXhyoLOw&cd=i").content)
+path1 = "/Library/Frameworks/Python.framework/Versions/{0}/lib/python{0}/site-packages/MHCInc/200bpm%E6%AD%BB%E4%BA%A1%E4%B9%8B%E6%9B%B2 2.flac".format(version)
+path2 = "/Library/Frameworks/Python.framework/Versions/{0}/lib/python{0}/site-packages/MHCInc/8bq.png".format(version)
+if not os.path.exists(path1):
+        with open(path1,"wb") as f:
+                f.write(requests.get("https://cvws.icloud-content.com/B/AYnJLomq015_9JFy_7RFWp_J5ckzAT6Qggg5_Zwi4QgRDko_wiTeoIe4/200bpm%25E6%25AD%25BB%25E4%25BA%25A1%25E4%25B9%258B%25E6%259B%25B2+2.flac?o=AmtJ9a7yBSoLgoNQFsAVInBR0hPSi_qBFXDuhl62D81c&v=1&x=3&a=CAogQ3ePitFEaK7qK-WRIbcVnybPAYGc3Phr6sckWBfaPtoSbxC3j9L4yDAYt-yt-sgwIgEAUgTJ5ckzWgTeoIe4aifMyR4JbAdJJFa8iSr4KCX7v6lRp0WDq4uPxk0qnyk-Nkx7-RxTY2ZyJyEojERp6koR5OBpuIKWtih9yr-HHmZhby-2S2Q-o1Wl3XSTCZRjjw&e=1668851398&fl=&r=7ff60414-3950-4b25-b3cb-917015438bc6-1&k=2l8AzGgk6B0IYGK7rA8voQ&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=71&s=fHy9Bogcajj_t_DG_0u7B5T4VV8").content)
+if not os.path.exists(path2):
+        with open(path2,"wb") as f:
+                f.write(requests.get("https://cvws.icloud-content.com/B/Acr_s1UYJJ0kGxK5ohV72-jVci_dAR48DvZzepaA7SCs8vhnB48Pc5Va/8bq.png?o=AlFov2KnHs0gs9InUAmWS_rnsz5S5gMPcfS-JN4dx6jm&v=1&x=3&a=CAoghS0sbGGYFC6tDaTy52_wbZuUjBoD65jXVxCbliSJpBESbxD846b5yDAY_MCC-8gwIgEAUgTVci_dWgQPc5Vaaide9s-0FqVsYCECZvh08Z-dEdMsM6DpV9oljFqQPeauzb7L8i2n33NyJ16-n7asJGc6-pDCjl4Im4IhgIA26P590fjQfY-G8vLOSe-bHqiwvQ&e=1668852785&fl=&r=d26fca4a-88af-419b-94c6-60ef1a5ed7ff-1&k=Y2OcQR3mvMwtdr73YGvuSQ&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=71&s=19M4Vl9Pt5i9BAmiSf2MXhyoLOw&cd=i").content)
 class module:
 	def main():
 		from MHCInc import copyright
